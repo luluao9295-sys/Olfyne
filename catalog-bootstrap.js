51fed16c0764fc13d30482280c30289b2dc55e5a
luluao9295-sys <lulu.ao9295@gmail.com>
2026-09-14 13:03:32 +0200

    Load eleventh verified official link batch

--- catalog-bootstrap.js
+++ catalog-bootstrap.js
@@ -20,7 +20,8 @@
       'data/official-links-7.json',
       'data/official-links-8.json',
       'data/official-links-9.json',
-      'data/official-links-10.json'
+      'data/official-links-10.json',
+      'data/official-links-11.json'
     ];
     const [perfumesResponse, metaResponse, ...linkResponses] = await Promise.all([
       nativeFetch(input, init),
